19bbe3c0c0f43a6065495adadb6fb527eb005231
Paul M. Watson <github.com@paulmwatson.com>
2019-10-03 15:32:28 +0200

    Update api_client_spec.rb
    Minor change from `expect(proc {` to just `expect {` as the test was failing.

--- spec/api_client_spec.rb
+++ spec/api_client_spec.rb
@@ -160,7 +160,7 @@ describe AylienNewsApi::ApiClient do
     end
 
     it "fails for invalid collection format" do
-      expect(proc { api_client.build_collection_param(param, :INVALID) }).to raise_error(RuntimeError, 'unknown collection format: :INVALID')
+      expect{ api_client.build_collection_param(param, :INVALID) }.to raise_error(RuntimeError, 'unknown collection format: :INVALID')
     end
   end
 
